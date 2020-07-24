efc89689bd735b11c394c6042f19ad99a885740a
Yasushi Ichikawa <ichikaway@gmail.com>
2020-07-24 11:54:58 +0900

    テストケース追加
    実際にlookupしてしまっているので、将来的にmockを使うように改善する

--- checker/checker_test.go
+++ checker/checker_test.go
@@ -19,3 +19,43 @@ func TestInArrayNotFound(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestCheckRecordNs(t *testing.T) {
+	var domain string = "vaddy.net"
+	var expect string = "ns-1151.awsdns-15.org. , ns-1908.awsdns-46.co.uk. , ns-457.awsdns-57.com. , ns-700.awsdns-23.net."
+
+	err := CheckRecord("NS", domain, expect)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestCheckRecordNsFail(t *testing.T) {
+	var domain string = "vaddy.net"
+	var expect string = "nstest.example.com , nstest2.example.com"
+
+	err := CheckRecord("NS", domain, expect)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestCheckRecordMx(t *testing.T) {
+	var domain string = "vaddy.net"
+	var expect string = "aspmx.l.google.com. , alt1.aspmx.l.google.com. , alt2.aspmx.l.google.com. , alt4.aspmx.l.google.com. , alt3.aspmx.l.google.com."
+
+	err := CheckRecord("MX", domain, expect)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestCheckRecordMxFail(t *testing.T) {
+	var domain string = "vaddy.net"
+	var expect string = "mxtest.example.com , mxtest2.example.com"
+
+	err := CheckRecord("MX", domain, expect)
+	if err == nil {
+		t.Fail()
+	}
+}
