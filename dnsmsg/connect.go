a6caf3a92cbf12136350fdfe1215d6ad1b4835eb
Yasushi Ichikawa <ichikaway@gmail.com>
2020-08-12 17:18:23 +0900

    DNSの再帰リクエストを送って結果を取得するまで実装

---
+++ dnsmsg/connect.go
@@ -0,0 +1,28 @@
+package dnsmsg
+
+import (
+	"fmt"
+	"net"
+)
+
+func Send(address string, message []byte) {
+	//conn, err := net.Dial("udp4", "127.0.0.1:10000")
+	conn, err := net.Dial("udp4", address)
+	if err != nil {
+			panic(err)
+	}
+	defer conn.Close()
+	fmt.Println("Sending to server")
+	_, err = conn.Write(message)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, err := conn.Read(buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Received: %s\n", string(buffer[:length]))
+}
---
+++ dnsmsg/connect_test.go
@@ -0,0 +1,38 @@
+package dnsmsg
+
+import (
+	"testing"
+)
+
+type A struct {
+	Int32Field int32
+	ByteField byte
+}
+/*
+func TestConnect(t *testing.T) {
+	//Send([]byte("Hello from Client"))
+	//Send([]byte{0xe3, 0x81, 0x82})
+	//a := A{Int32Field: 0x123456, ByteField: 0xFF}
+	//a := A{Int32Field: 0xe38182, ByteField: 0xFF}
+
+	//a := try("www.vaddy.net")
+	a := try("a.net")
+	fmt.Printf("%v\n",a)
+	b := bytes.NewBuffer([]byte(""))
+
+	// 構造体をバイナリにする
+	binary.Write(b, binary.BigEndian, &a)
+	//binary.Write(b, binary.LittleEndian, &a)
+	fmt.Printf("%x\n",b.Bytes())
+	Send(b.Bytes())
+}
+*/
+func TestConnect(t *testing.T) {
+	id := uint16(60000)
+	h := NewHeader(id)
+	header := NewHeaderPayload(h)
+	q := NewQuestion("vaddy.net", TypeNS)
+	question := NewQuestionPayload(q)
+	req := append(header, question...)
+	Send("192.168.12.1:53", req)
+}
