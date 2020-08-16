c90c3a4f17147fc43fbffe248fe7c74590b9f2a7
Yasushi Ichikawa <ichikaway@gmail.com>
2020-08-16 16:00:33 +0900

    go fmt

--- misc/bin.go
+++ misc/bin.go
@@ -9,8 +9,8 @@ const str2 string = "\x03www\x04jp0rs\x02co\x02jp\x00\x00\x01\x00\x01"
 
 func checkNullByte() {
 	//fmt.Printf("%x", str)
-	for i,s := range []byte(str2) {
-		fmt.Printf("%d:%x ", i,s)
+	for i, s := range []byte(str2) {
+		fmt.Printf("%d:%x ", i, s)
 		if s == 0 {
 			fmt.Printf("\nMatch nullbyte")
 		}
