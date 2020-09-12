b6797de6a0da3dc6b39825e16d18574bdf3d9ab2
Yasushi Ichikawa <ichikaway@gmail.com>
2020-09-12 18:22:53 +0900

    refactoring read dns response data

--- dnsmsg/response.go
+++ dnsmsg/response.go
@@ -5,6 +5,9 @@ import (
 	"errors"
 )
 
+//char of dots. ascii hex
+const dot byte = 0x2e
+
 func getNsListFromDnsResponse(message []byte) ([]string, error) {
 	const headerByteLen int = 12
 	const typeByteLen int = 2
@@ -70,7 +73,6 @@ func getNsListFromDnsResponse(message []byte) ([]string, error) {
 // name ex. vaddy.net
 // readByte means read byte size
 func readName(message []byte, readCounter int) (name string, readByte int, err error) {
-	const dot byte = 0x2e
 	const nullByteLen int = 1
 
 	data := message[readCounter:]
@@ -78,10 +80,7 @@ func readName(message []byte, readCounter int) (name string, readByte int, err e
 	var nameByte []byte = make([]byte, 0, 50)
 	for readByte, byteData := range data {
 		if byteData == 0x00 {
-			if nameByte[0] == dot {
-				//先頭のドットは不要
-				nameByte = nameByte[1:]
-			}
+			nameByte = dropDotInHeadByte(nameByte)
 			name = string(nameByte)
 			if nameByte[len(nameByte)-1] != dot {
 				//最後がドットで終わってなければドットを付与する
@@ -89,25 +88,16 @@ func readName(message []byte, readCounter int) (name string, readByte int, err e
 			}
 			return name, readByte + nullByteLen, nil
 		}
-		if labelCount == 0 {
-			//label count0の場合はラベルの数字のため、圧縮されていないか確認
-			if byteData > 63 {
-				//圧縮先の参照データの先頭からのバイト数を取得
-				//  upperの上位2ビットを落とす
-				//  underの8bitとupperを8shiftしたものを足したint16の数
-				var upper int16 = int16(byteData & 0b00111111)
-				var under int16 = int16(data[readByte+1])
-				var compressedCounter int16 = upper<<8 + under
-				compressedNameString, _, err := readName(message, int(compressedCounter)) //圧縮先で読んだバイト数は不要
+		if labelCount == 0 { //label count0の場合はラベルの数字のため、圧縮されていないか確認
+			if needCheckCompression(byteData) {
+				compressedCounter := getFragmentPointer(byteData, data[readByte+1])  //現在の読込バイトと、次の読込予定バイトを使って圧縮先のバイト数を取得
+				compressedNameString, _, err := readName(message, compressedCounter) //圧縮先のデータを再帰で取得。圧縮先で読んだバイト数は不要
 				if err != nil {
 					return "", 0, err
 				}
-				if nameByte[0] == dot {
-					//先頭のドットは不要
-					nameByte = nameByte[1:]
-				}
+				nameByte = dropDotInHeadByte(nameByte)
 				name = string(nameByte) + string(dot) + compressedNameString
-				readByte += 2 //label文字数とフラグメントのポインタのバイトを足す
+				readByte += 2 //label文字数を示す1バイトと、フラグメントのポインタを示す1バイトを読込済みとして足しておく
 				return name, readByte, nil
 			}
 
@@ -120,6 +110,29 @@ func readName(message []byte, readCounter int) (name string, readByte int, err e
 		}
 	}
 	return "", 0, errors.New("No terminate byte.")
+}
+
+//圧縮先の参照データの先頭からのバイト数を取得
+func getFragmentPointer(first byte, second byte) int {
+	//  upperの上位2ビットを落とす
+	//  underの8bitとupperを8shiftしたものを足したint16の数
+	var upper int16 = int16(first & 0b00111111)
+	var under int16 = int16(second)
+	return int(upper<<8 + under)
+}
+
+//先頭ビットが11で始まる0b11000000(63)以上の場合は名前圧縮されているためその判定を行う関数
+func needCheckCompression(byteData byte) bool {
+	return byteData > 63
+}
+
+//名前配列の先頭がドットだった場合は先頭のドットを切り捨てる
+func dropDotInHeadByte(nameByte []byte) []byte {
+	if nameByte[0] == dot {
+		//先頭のドットは不要
+		nameByte = nameByte[1:]
+	}
+	return nameByte
 }
 
 // こちらで生成したDNS IDとレスポンスでセットされたDNS IDが一致するかチェックする
